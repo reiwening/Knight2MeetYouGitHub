2b29ef16a76272501d15270d4cbcf276f945a10d
Reiwen <reiwen.ing.2023@scis.smu.edu.sg>
2024-09-25 23:15:23 +0800

    added User and all entities done

--- src/main/java/com/g5/cs203proj/entity/User.java
+++ src/main/java/com/g5/cs203proj/entity/User.java
@@ -1,5 +1,39 @@
 package com.g5.cs203proj.entity;
 
+import java.util.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    private String username;
+    private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
+    private String hashedPassword;
+    
+
+    public User(String username, Long id, String hashedPassword) {
+        this.username = username;
+        this.id = id;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public User() {
+        
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+
+
     
 }
