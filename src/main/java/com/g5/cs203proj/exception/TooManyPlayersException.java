bd926c943a1adc5d82a09efafc07dfb4d4d0ba78
daikon-radish <chooyangh@gmail.com>
2024-11-03 23:59:07 +0800

    working round robin implementation

---
+++ src/main/java/com/g5/cs203proj/exception/TooManyPlayersException.java
@@ -0,0 +1,13 @@
+package com.g5.cs203proj.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class TooManyPlayersException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public TooManyPlayersException(int numPlayers) {
+        super("The tournament currently has " + numPlayers + " players. The maximum allowed for a round-robin format is 16.");
+    }
+}
