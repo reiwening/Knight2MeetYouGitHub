fb7a37c320fd92b9a1f2c020c7e0c33febaae0e2
Reiwen <reiwen.ing.2023@scis.smu.edu.sg>
2024-11-07 04:12:15 +0800

    added testing code

--- src/main/java/com/g5/cs203proj/entity/Admin.java
+++ src/main/java/com/g5/cs203proj/entity/Admin.java
@@ -1,44 +1,44 @@
-package com.g5.cs203proj.entity;
+// package com.g5.cs203proj.entity;
 
-import java.util.*;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+// import java.util.*;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.OneToMany;
 
-@Entity
-public class Admin {
+// @Entity
+// public class Admin {
 
 
-    private String username;
-    private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
-    private String hashedPassword;
+//     private String username;
+//     private @Id @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
+//     private String hashedPassword;
 
-    @OneToMany(mappedBy = "admin")
-    private List<Tournament> tournaments;  // One admin can manage multiple tournaments
+//     @OneToMany(mappedBy = "admin")
+//     private List<Tournament> tournaments;  // One admin can manage multiple tournaments
 
-    // Getters and Setters for tournaments
+//     // Getters and Setters for tournaments
 
-    public List<Tournament> getTournaments() {
-        return tournaments;
-    }
+//     public List<Tournament> getTournaments() {
+//         return tournaments;
+//     }
 
-    public Admin(String username, String hashedPassword) {
-        this.username = username;
-        this.hashedPassword = hashedPassword;
-    }
+//     public Admin(String username, String hashedPassword) {
+//         this.username = username;
+//         this.hashedPassword = hashedPassword;
+//     }
 
-    public void setTournaments(List<Tournament> tournaments) {
-        this.tournaments = tournaments;
-    }
+//     public void setTournaments(List<Tournament> tournaments) {
+//         this.tournaments = tournaments;
+//     }
 
-    public String getUsername() {
-        return username;
-    }
+//     public String getUsername() {
+//         return username;
+//     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
 
-}
+// }
