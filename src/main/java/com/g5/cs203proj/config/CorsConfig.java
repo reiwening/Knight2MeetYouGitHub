47109ffcb30d1a8a592a6210c770f170b881a596
daikon-radish <chooyangh@gmail.com>
2024-11-10 17:27:12 +0800

    changed frontend config to allow bot

--- src/main/java/com/g5/cs203proj/config/CorsConfig.java
+++ src/main/java/com/g5/cs203proj/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.g5.cs203proj.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*") // Enable CORS for all routes
+        registry.addMapping("/**") // Enable CORS for all routes
                 .allowedOrigins("http://localhost:3000") // React frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("")
+                .allowedHeaders("*") // Allow all headers
+                .exposedHeaders("Authorization", "Content-Type") // Expose any additional headers if needed
                 .allowCredentials(true);
     }
 }
